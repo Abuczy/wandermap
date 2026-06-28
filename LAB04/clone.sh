1e7a49bbb5c46882fc68297943802bc5d902b5a1
oliwkata <167431476+oliwkata@users.noreply.github.com>
2026-06-28 21:55:01 +0200

    Delete LAB04 directory

--- LAB04/clone.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$1" == "--init" ]; then
-	git clone https://github.com/oliwkata/LAB04 . && \
-	export PATH=$PATH:$(pwd)
-	echo "Sklonowano repozytorium i dodano PATH"
-	exit 0
-fi
-
